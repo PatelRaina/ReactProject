24304735611124c99a06667f597c20960872e007
PatelRaina <76577728+PatelRaina@users.noreply.github.com>
2023-03-14 23:54:26 -0400

    Delete frontend/src directory

--- frontend/src/shared/Subtitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import '../styles/home.css';
-
-const Subtitle = ({subtitle}) => {
-    return <h3 className="section__subtitle">{subtitle}</h3>
-    
-};
- 
-export default Subtitle;
